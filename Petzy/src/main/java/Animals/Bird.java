5cae7f1f3110b97c08a383e06d82bb2334c81587
Borbély Viktor <borbelyviktor731@gmail.com>
2025-04-24 14:24:00 +0200

    i cant

--- Petzy/src/main/java/Animals/Bird.java
+++ Petzy/src/main/java/Animals/Bird.java
@@ -2,8 +2,8 @@ package Animals;
 
 import java.io.Serializable;
 
-public class Frog extends Animal implements Serializable {
-  public Frog(String petName) {
+public class Bird extends Animal implements Serializable {
+  public Bird(String petName) {
     super(petName, 100, 0, 100, 100);  // hunger starts at 0
   }
 }
